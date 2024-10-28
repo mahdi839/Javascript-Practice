10d00e0bd9e9a5d88add23cb72fa762706ed3604
mahdi839 <77610054+mahdi839@users.noreply.github.com>
2024-10-28 22:23:02 +0600

    updated

--- Maps/maps.js
+++ Maps/maps.js
@@ -4,5 +4,29 @@ let mapsItams = new Map ([
     ['moon','sister'],
     ['tasnu','whife']
 ])
-mapsItams.set('robin','brother')
-console.log(mapsItams.get(24))
+// for(let [key,val] of mapsItams){
+//     console.log(key,val)
+// }
+
+// mapsItams.delete('tasnu')
+// let MapSize = mapsItams.size
+// console.log(MapSize)
+// mapsItams.clear()
+// console.log(mapsItams)
+// let check = mapsItams.has('moon')
+// console.log(check)
+
+// console.log(mapsItams.keys())
+// console.log(mapsItams.values())
+const fruits = new Map();
+const apples = {name: 'Apples'};
+const bananas = {name: 'Bananas'};
+const oranges = {name: 'Oranges'};
+
+
+// Add the Objects to the Map
+fruits.set(apples, 500);
+fruits.set(bananas, 300);
+fruits.set(oranges, 200);
+console.log(fruits)
+
